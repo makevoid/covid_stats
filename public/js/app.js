7438d07dfaf2825008a3099718519e6fcd81a034
makevoid <makevoid@gmail.com>
2020-10-27 23:25:23 +0000

    update api key maps

--- public/js/app.js
+++ public/js/app.js
@@ -20,9 +20,7 @@ const drawRegionsMap = () => {
 window.addEventListener('DOMContentLoaded', (event) => {
   google.charts.load('current', {
     'packages':['geochart'],
-    // Note: you will need to get a mapsApiKey for your project.
-    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+    'mapsApiKey': 'AIzaSyBmkSJKJhWnSJNVHS33kocZy6YC8pVJJCk'
   })
   google.charts.setOnLoadCallback(drawRegionsMap)
 })
