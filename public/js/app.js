1e154de8bed982906919af3c814a7407339f55e8
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2020-10-27 12:06:39 +0000

    bundle update, recolor and optimize maps, remove navbar lnks

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,6 @@
-google.charts.load('current', {
-  'packages':['geochart'],
-  // Note: you will need to get a mapsApiKey for your project.
-  // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-  'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-})
-google.charts.setOnLoadCallback(drawRegionsMap)
 
-function drawRegionsMap() {
+
+const drawRegionsMap = () => {
   // window.data_7d_deaths.prepend['Country', '# of deaths - 7 days']
   const data = google.visualization.arrayToDataTable(
     window.data_7d_deaths
@@ -14,9 +8,21 @@ function drawRegionsMap() {
 
   const options = {
     region: '150',
+    colorAxis: { colors: ["orange"] },
   }
-  const chartElem = document.querySelector('#regions_div')
+  const chartElem = document.querySelector('.regions_div')
   const chart = new google.visualization.GeoChart(chartElem)
 
   chart.draw(data, options)
 }
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  google.charts.load('current', {
+    'packages':['geochart'],
+    // Note: you will need to get a mapsApiKey for your project.
+    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+  })
+  google.charts.setOnLoadCallback(drawRegionsMap)
+})
